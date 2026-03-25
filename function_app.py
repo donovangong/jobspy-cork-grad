6a944f2a5a1b6be9d82d2346ac11acb0d3d018ef
donovangong <gongchenghan@gmail.com>
2026-03-25 00:38:47 +0000

    Fix indentation for raise statement in timer trigger

--- function_app.py
+++ function_app.py
@@ -5,9 +5,6 @@ from job_logic import run_pipeline
 
 app = func.FunctionApp()
 
-# Azure Functions 的定时表达式是 6 段：
-# second minute hour day month day-of-week
-# 下面这个表示：每天 UTC 08:10 跑一次
 @app.timer_trigger(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=False, use_monitor=True)
 def daily_jobspy_runner(mytimer: func.TimerRequest) -> None:
     logging.info("JobSpy timer trigger started.")
